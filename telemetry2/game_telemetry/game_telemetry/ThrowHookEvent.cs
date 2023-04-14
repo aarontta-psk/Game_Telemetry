9a5ab7587f1d87f15e490499290a23c5ebe6dd91
Samuel <samuebla@ucm.es>
2023-04-14 10:35:22 +0200

    Prueba Evento creado

---
+++ telemetry2/game_telemetry/game_telemetry/ThrowHookEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_telemetry
+{
+    class ThrowHookEvent: TelemetryEvent
+    {
+        int posPlayerX;
+        int posPlayerY;
+        int posMouseX;
+        int posMouseY;
+        int numHooks;
+        protected ThrowHookEvent(EventType type, string session_id, int posPlayerXAux,int posPlayerYAux,int posMouseXAux,int posMouseYAux,int numHooksAux) : base(type, session_id)
+        {
+            posPlayerX = posPlayerXAux;
+            posPlayerY = posPlayerYAux;
+            posMouseX = posMouseXAux;
+            posMouseY = posMouseYAux;
+            numHooks = numHooksAux;
+        }
+    }
+}
